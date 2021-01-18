72de80008a608c5723c76a77454132a17ad45e85
Krzysztof Pustulka <polkestrel@gmail.com>
2021-01-18 10:47:06 +0100

    Instrukcje warunkowe - zadanie

---
+++ java-basics/src/AgeChecker.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AgeChecker {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj swój wiek");
+        float age = scanner.nextFloat();
+
+        if (age >= 18) {
+            System.out.println("Twój wiek to 18+, a to oznacza, że możesz kupić alkohol");
+        } else if (age < 18 && age > 0) {
+            System.out.println("Masz poniżej 18 lat. Nie możesz kupic alkoholu");
+        } else {
+            System.out.println("Hej! Nie wygłupiaj się - podaj jeszcze raz swój wiek.");
+        }
+    }
+}
