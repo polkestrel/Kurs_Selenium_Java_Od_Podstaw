eb166cdb0bdfc40cb5b12da0102d175578b0113a
Krzysztof Pustulka <polkestrel@gmail.com>
2021-01-18 20:50:42 +0100

    Kalkulator - zadanie

--- java-basics/src/Calculator.java
+++ java-basics/src/Calculator.java
@@ -11,16 +11,14 @@ public class Calculator {
         float firstNumber = scanner.nextFloat();
         System.out.println("Teraz podaj drugą dowolną liczbę...");
         float secondNumber = scanner.nextFloat();
-        float summation = firstNumber + secondNumber;
-        float subtraction = firstNumber - secondNumber;
-        float division = firstNumber / secondNumber;
-        float multiplication = firstNumber * secondNumber;
-        float mod = firstNumber % secondNumber;
-        System.out.println("Suma podanych liczb (czyli " + firstNumber + " + " + secondNumber + ") wynosi: " + summation);
-        System.out.println("Różnica podanych liczb (czyli " + firstNumber + " - " + secondNumber + ") wynosi: " + subtraction);
-        System.out.println("Iloraz podanych liczb (czyli " + firstNumber + " / " + secondNumber + ") wynosi: " + division);
-        System.out.println("Iloczyn podanych liczb (czyli " + firstNumber + " * " + secondNumber + ") wynosi: " + multiplication);
-        System.out.println("Reszta z dzielenia podanych liczb (czyli " + firstNumber + " % " + secondNumber + ") wynosi: " + mod);
+
+        CalculatorTest calculator = new CalculatorTest();
+
+        System.out.println("Suma podanych liczb (czyli " + firstNumber + " + " + secondNumber + ") wynosi: " + calculator.summation(firstNumber,secondNumber));
+        System.out.println("Różnica podanych liczb (czyli " + firstNumber + " - " + secondNumber + ") wynosi: " + calculator.subtraction(firstNumber,secondNumber));
+        System.out.println("Iloraz podanych liczb (czyli " + firstNumber + " / " + secondNumber + ") wynosi: " + calculator.division(firstNumber,secondNumber));
+        System.out.println("Iloczyn podanych liczb (czyli " + firstNumber + " * " + secondNumber + ") wynosi: " + calculator.mulltiplication(firstNumber,secondNumber));
+        System.out.println("Reszta z dzielenia podanych liczb (czyli " + firstNumber + " % " + secondNumber + ") wynosi: " + calculator.mod(firstNumber,secondNumber));
         System.out.println("Dzięki! To na razie tyle.");
     }
 }
---
+++ java-basics/src/CalculatorTest.java
@@ -0,0 +1,22 @@
+public class CalculatorTest {
+
+    public float summation(float firstNumber, float secondNumber) {
+        return firstNumber+secondNumber;
+    }
+
+    public float subtraction(float firstNumber, float secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public float division(float firstNumber, float secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    public float mulltiplication(float firstNumber, float secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public float mod(float firstNumber, float secondNumber) {
+        return firstNumber % secondNumber;
+    }
+}
