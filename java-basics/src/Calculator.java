0553b0b482f0be9882c47fdb41f0ff1e24221758
Krzysztof Pustulka <polkestrel@gmail.com>
2021-01-18 09:56:41 +0100

    Kalkulator

--- java-basics/src/Calculator.java
+++ java-basics/src/Calculator.java
@@ -15,10 +15,12 @@ public class Calculator {
         float subtraction = firstNumber - secondNumber;
         float division = firstNumber / secondNumber;
         float multiplication = firstNumber * secondNumber;
+        float mod = firstNumber % secondNumber;
         System.out.println("Suma podanych liczb (czyli " + firstNumber + " + " + secondNumber + ") wynosi: " + summation);
         System.out.println("Różnica podanych liczb (czyli " + firstNumber + " - " + secondNumber + ") wynosi: " + subtraction);
         System.out.println("Iloraz podanych liczb (czyli " + firstNumber + " / " + secondNumber + ") wynosi: " + division);
         System.out.println("Iloczyn podanych liczb (czyli " + firstNumber + " * " + secondNumber + ") wynosi: " + multiplication);
+        System.out.println("Reszta z dzielenia podanych liczb (czyli " + firstNumber + " % " + secondNumber + ") wynosi: " + mod);
         System.out.println("Dzięki! To na razie tyle.");
     }
 }
