12b7aa0d16dd0e06fc7aa51ecd69de23a771f461
Krzysztof Pustulka <polkestrel@gmail.com>
2021-01-20 11:34:48 +0100

    Method overloading - przykład

---
+++ java-basics/src/MethodNameTest.java
@@ -0,0 +1,11 @@
+public class MethodNameTest {
+
+    public static void main(String[] args) {
+        MethodName methodName = new MethodName();
+        methodName.add();
+        methodName.add(1);
+        methodName.add("a");
+        methodName.add(1,4);
+        methodName.add(1,3,6);
+    }
+}
