b227cbcbef100c8b96cb841d42f05fc5007b1fff
Krzysztof Pustulka <polkestrel@gmail.com>
2021-01-21 14:17:41 +0100

    Konwersja i rzutowanie typów - przykłady

---
+++ java-basics/src/ConversionCasting.java
@@ -0,0 +1,22 @@
+import drivers.FirefoxDriver;
+import drivers.WebDriver;
+
+public class ConversionCasting {
+
+    public static void main(String[] args) {
+
+        int a = 2;
+        double b = 4.23;
+
+        double c = a/b; // 'a' zostaje przekonwertowana na double
+
+        int d = a/(int)b; // 'b' zostaje przekonwertowana na typ int
+
+        System.out.println(c);
+        System.out.println(d);
+
+        WebDriver driver = new FirefoxDriver();
+        FirefoxDriver firefoxDriver = (FirefoxDriver) driver;
+        firefoxDriver.get();
+    }
+}
