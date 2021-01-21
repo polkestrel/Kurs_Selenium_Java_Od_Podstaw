624b951371c35fc423ea8c5422ddaa336704817c
Krzysztof Pustulka <polkestrel@gmail.com>
2021-01-21 14:52:15 +0100

    Obsługa wyjątków za pomocą 'try & catch' oraz 'throws' (exceptions) oraz slowo 'finally'- przykład

--- java-basics/src/CheckedExceptions.java
+++ java-basics/src/CheckedExceptions.java
@@ -3,8 +3,17 @@ import java.io.FileNotFoundException;
 
 public class CheckedExceptions {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        readFile("src/sdsd.txt");
+    public static void main(String[] args) {
+        try {
+            System.out.println("Before reading file");
+            readFile("sds.txt");
+            //readFile("C:\\SelfLearning\\Udemy\\Kurs Selenium od podstaw\\Selenium-course\\java-basics\\src\\test.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("Wyjątek został wyrzucony");
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Closing file");
+        }
     }
 
     public static void readFile(String path) throws FileNotFoundException {
