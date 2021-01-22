a332903e49fbc42dff3c69a7f9e23b590299f363
Krzysztof Pustulka <polkestrel@gmail.com>
2021-01-22 20:29:48 +0100

    Listy - przykłady

---
+++ java-basics/src/listy/ListyTest.java
@@ -0,0 +1,52 @@
+package listy;
+
+import docs.PdfDocument;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListyTest {
+
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+
+        names.add("Paweł");
+        names.add("Kasia");
+        names.add("Ola");
+        names.add("Paweł");
+
+        System.out.println(names.get(1));
+        System.out.println(names.indexOf("Paweł"));
+        System.out.println(names.lastIndexOf("Paweł"));
+        System.out.println(names.contains("Paweł"));
+        System.out.println(names.size());
+        System.out.println(names.isEmpty());
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+
+        System.out.println(numbers.get(1));
+        System.out.println(numbers.indexOf(2));
+        System.out.println(numbers.lastIndexOf(4));
+        System.out.println(numbers.contains(2));
+        System.out.println(numbers.size());
+        System.out.println(numbers.isEmpty());
+
+        List<String> diffNames = new ArrayList<>();
+        diffNames.addAll(names);
+
+        List<PdfDocument> pdfDocuments = new ArrayList<>();
+        pdfDocuments.add(new PdfDocument());
+
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i));
+        }
+
+        for (String sss : names) {
+            System.out.println(sss);;
+        }
+    }
+}
