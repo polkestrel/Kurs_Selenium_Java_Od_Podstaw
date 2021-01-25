ad96539bfaf6eb38bceb85f608346af4ec52e31f
Krzysztof Pustulka <polkestrel@gmail.com>
2021-01-25 10:36:46 +0100

    Wyszukiwanie elementów po ID

--- selenium/src/test/java/FirstSeleniumTest.java
+++ selenium/src/test/java/FirstSeleniumTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstSeleniumTest {
@@ -10,12 +11,9 @@ public class FirstSeleniumTest {
     public void googleOpenTest() {
         String driverPath = "C:\\SelfLearning\\Udemy\\Kurs Selenium od podstaw\\Selenium-course\\selenium\\src\\main\\resources\\executables\\drivers\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver",driverPath);
-        Dimension dimension = new Dimension(1900,800);
         WebDriver driver = new ChromeDriver();
-        driver.manage().window().setSize(dimension);
+        driver.manage().window().maximize();
         driver.get("C:\\SelfLearning\\Udemy\\Kurs Selenium od podstaw\\docs\\Test.html");
-        driver.findElement(By.id("newPage")).click();
-        driver.quit(); // zamuka wszystkie okna
-        //driver.close(); // zamyka pierwotne okno
+        WebElement firstNameInput = driver.findElement(By.id("fname"));
     }
 }
