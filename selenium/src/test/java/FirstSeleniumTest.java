4bfc5ff8664aff2ba9402b8173c0fe45618011f6
Krzysztof Pustulka <polkestrel@gmail.com>
2021-01-25 18:46:07 +0100

    Wyszukiwanie elementów na stronie - update

--- selenium/src/test/java/FirstSeleniumTest.java
+++ selenium/src/test/java/FirstSeleniumTest.java
@@ -14,13 +14,18 @@ public class FirstSeleniumTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("C:\\SelfLearning\\Udemy\\Kurs Selenium od podstaw\\docs\\Test.html");
-        WebElement clickOnMeButton = driver.findElement(By.id("clickOnMe"));
+        // WebElement clickOnMeButton = driver.findElement(By.id("clickOnMe"));
         WebElement firstNameInput = driver.findElement(By.name("fname"));
         WebElement w3schoolLink = driver.findElement(By.linkText("Visit W3Schools.com!"));
         WebElement googleLink = driver.findElement(By.partialLinkText("Weird"));
         WebElement topSecretParagraph = driver.findElement(By.className("topSecret"));
         WebElement firstInput = driver.findElement(By.tagName("input"));
         WebElement firstLink = driver.findElement(By.tagName("a"));
+
+        WebElement clickOnMeButton = driver.findElement(By.cssSelector("#clickOnMe"));
+        WebElement links = driver.findElement(By.cssSelector("a"));
+        WebElement topSecretCss = driver.findElement(By.cssSelector(".topSecret"));
+        WebElement tdFirstChild = driver.findElement(By.cssSelector("td:first-child"));
 
     }
 }
