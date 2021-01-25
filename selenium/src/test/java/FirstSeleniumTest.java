78b9ad7f9d13972d59c85ba70b015d4371b07aa8
Krzysztof Pustulka <polkestrel@gmail.com>
2021-01-25 09:58:21 +0100

    Pierwszy test w Selenium - zmiana wielkości okna przeglądarki

--- selenium/src/test/java/FirstSeleniumTest.java
+++ selenium/src/test/java/FirstSeleniumTest.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,7 +9,9 @@ public class FirstSeleniumTest {
     public void googleOpenTest() {
         String driverPath = "C:\\SelfLearning\\Udemy\\Kurs Selenium od podstaw\\Selenium-course\\selenium\\src\\main\\resources\\executables\\drivers\\chromedriver.exe";
         System.setProperty("webdriver.chrome.driver",driverPath);
+        Dimension dimension = new Dimension(1900,800);
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().setSize(dimension);
         driver.get("https://www.google.com");
     }
 }
